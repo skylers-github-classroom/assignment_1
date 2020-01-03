8de8105e5c7669a884ecba1baa97572889f3bada
Skyler Taylor <skylert@splunk.com>
2020-01-03 14:09:58 -0800

    adding failing test

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ from module_1 import Add
 def test_add():
     me = Add()
     assert(me.add_correct(1, 2) == 3)
+    assert(me.add_incorrect(1, 2) == 3)
 
 if __name__ == "__main__":
     test_add()
